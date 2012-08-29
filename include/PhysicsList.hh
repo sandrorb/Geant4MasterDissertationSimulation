134e4bf61ceca5671d44c8135e6ba4c94946a7dd
sandrorb <sandrorb@gmail.com>
2012-08-28 22:08:38 -0300

    Additional commit because the previous one reported an error.

--- include/PhysicsList.hh
+++ include/PhysicsList.hh
@@ -1,9 +1,3 @@
-/**
- * @author Sandro Boschetti
- *
- * This is a class based on ExN01PhysicsList.hh from Geant4.9.3.p02
- *
- */
 
 #ifndef PhysicsList_h
 #define PhysicsList_h 1
@@ -11,19 +5,38 @@
 #include "G4VUserPhysicsList.hh"
 #include "globals.hh"
 
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
 class PhysicsList: public G4VUserPhysicsList
 {
   public:
-	PhysicsList();
-    ~PhysicsList();
+    PhysicsList();
+   ~PhysicsList();
 
   protected:
-    // Construct particle and physics process
+    // Construct particle and physics
     void ConstructParticle();
     void ConstructProcess();
+ 
     void SetCuts();
 
+   
+  protected:
+    // these methods Construct particles 
+    void ConstructBosons();
+    void ConstructLeptons();
+    void ConstructMesons();
+    void ConstructBaryons();
+
+  protected:
+  // these methods Construct physics processes and register them
+    void ConstructGeneral();
+    void ConstructEM();
+    void AddStepMax();
 };
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
 #endif
 
+ 
