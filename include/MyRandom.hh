2b0a4a92c513b64ea03796e4b335462ba0f991a4
Geant4 by CERN <geant4@localhost.localdomain>
2013-06-14 11:41:47 -0300

    Some changes to be able to compile using cmake and CMakelists.

--- include/MyRandom.hh
+++ include/MyRandom.hh
@@ -5,13 +5,14 @@
 #define randomize_h 1
 
 	#include <CLHEP/Random/Randomize.h>
-
+/*
 	#include <CLHEP/Random/RandGaussQ.h>
 	#include <CLHEP/Random/RandGaussT.h>
 	#include <CLHEP/Random/RandPoissonQ.h>
 	#include <CLHEP/Random/RandPoissonT.h>
 	#include <CLHEP/Random/RandLandau.h>
 	#include <CLHEP/Random/RandBit.h>
+*/
 
 	#define G4RandGauss RandGaussQ
 	//#define G4UniformRand() HepRandom::getTheEngine()->flat()
