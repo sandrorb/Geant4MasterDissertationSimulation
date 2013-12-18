16faf9388a66658c886ae06c314bd36335e0a658
sandrorb <sandrorb@gmail.com>
2013-12-18 21:53:51 -0200

    To complete the previous commit.

--- Geant4MasterDissertationSimulation.cc
+++ Geant4MasterDissertationSimulation.cc
@@ -173,7 +173,7 @@ int main(G4int argc, char** argv) {
 	 *@endcode
 	 *
 	 */
-	//G4UIExecutive* session = new G4UIExecutive(argc, argv);
+	G4UIExecutive* session = new G4UIExecutive(argc, argv);
 
 	/**
 	 * @code
@@ -186,7 +186,7 @@ int main(G4int argc, char** argv) {
 	 * information for the simulation.
 	 *
 	 */
-//	G4UImanager* UI = G4UImanager::GetUIpointer();
+	G4UImanager* UI = G4UImanager::GetUIpointer();
 
 	/**
 	 * @code
@@ -195,7 +195,7 @@ int main(G4int argc, char** argv) {
 	 * Local function implemented for a sake of organization.
 	 * This function sets up some UI characteristics.
 	 */
-//	setupUIProgramatically(UI);
+	setupUIProgramatically(UI);
 
 	 // This gets the actual time. Used to compute the simulation time.
 	time_t timeAtBegin = time(0);
@@ -221,8 +221,8 @@ int main(G4int argc, char** argv) {
 	 * Related to G4UIExecutive class
 	 */
 	// Starts interactive session
-//	session->SessionStart();
-//	delete session;
+	session->SessionStart();
+	delete session;
 
 	#ifdef G4VIS_USE
 		delete visManager;
@@ -258,5 +258,6 @@ void setupUIProgramatically(G4UImanager *UI){
 	//UI->ApplyCommand("/tracking/storeTrajectory");
 	//UI->ApplyCommand("/vis/viewer/set/style surface"); //wireframe or surface
 	//UI->ApplyCommand("/tracking/storeTrajectory 20");
-	UI->ApplyCommand("/vis/scene/endOfEventAction accumulate 30");
+	UI->ApplyCommand("/vis/scene/endOfEventAction accumulate 100");
+	UI->ApplyCommand("/vis/viewer/set/whiteBackground");
 }
